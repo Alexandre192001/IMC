f300531d89c4ede6f53f32c79fe44c144210f900
Alexandre Júnior <alexandrepj13@gmail.com>
2022-11-22 15:52:13 -0300

    Create dark and Ligth project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,63 +1,71 @@
+// DARK/LIGTH
+const dark = document.querySelector(".dark-ligth-wrapper")
 
-/*Botão de calculo IMC*/ 
+dark.addEventListener("click", () => {
+  document.querySelector(".bi-sun-fill").classList.toggle("dark-ligth")
+  document.querySelector(".container-main").classList.toggle("dark-container")
+  document.querySelector(".titulo-section").classList.toggle("title-dark")
+})
+
+
+/*Botão de calculo IMC*/
 document.querySelector(".btn_imc").addEventListener("click",
-()=>{
-  /*Valores de peso e altura*/
-  let peso = document.getElementById("peso").value;
-  let altura = document.getElementById("altura").value/100;
-  const imc =(peso/altura**2).toFixed(1);
-  if(peso=="" || altura==""){
-    window.alert("Preencha todos os campos")
-  } else{
-  if(imc<18.5){
-    document.querySelector(".resultado-peso").innerHTML =`
+  () => {
+    /*Valores de peso e altura*/
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value / 100;
+    const imc = (peso / altura ** 2).toFixed(1);
+    if (peso == "" || altura == "") {
+      window.alert("Preencha todos os campos")
+    } else {
+      if (imc < 18.5) {
+        document.querySelector(".resultado-peso").innerHTML = `
       IMC : ${imc}
     `;
-    document.querySelector(".resultado-situacao").innerHTML = `
+        document.querySelector(".resultado-situacao").innerHTML = `
     Situação - Abaixo do peso
     `
-    } else if(imc<25){
-      document.querySelector(".resultado-peso").innerHTML =`
+      } else if (imc < 25) {
+        document.querySelector(".resultado-peso").innerHTML = `
       IMC : ${imc}
     `;
-    document.querySelector(".resultado-situacao").innerHTML = `
+        document.querySelector(".resultado-situacao").innerHTML = `
     Situação - Peso normal
     `
-    } else if(imc<30){
-      document.querySelector(".resultado-peso").innerHTML =`
+      } else if (imc < 30) {
+        document.querySelector(".resultado-peso").innerHTML = `
       IMC : ${imc}
       
     `;
-    document.querySelector(".resultado-situacao").innerHTML = `
+        document.querySelector(".resultado-situacao").innerHTML = `
     Situação - Sobrepeso
     `
-    } else if(imc<35){
-      document.querySelector(".resultado-peso").innerHTML =`
+      } else if (imc < 35) {
+        document.querySelector(".resultado-peso").innerHTML = `
       IMC : ${imc}
     `;
-    document.querySelector(".resultado-situacao").innerHTML = `
+        document.querySelector(".resultado-situacao").innerHTML = `
     Situação - Obesidade grau I
     `
 
-    } else if(imc<40){
-      document.querySelector(".resultado-peso").innerHTML =`
+      } else if (imc < 40) {
+        document.querySelector(".resultado-peso").innerHTML = `
       IMC : ${imc}
     `;
-    document.querySelector(".resultado-situacao").innerHTML = `
+        document.querySelector(".resultado-situacao").innerHTML = `
     Situação - Obesidade grau II
     `
-    }
-    else{
-      document.querySelector(".resultado-peso").innerHTML =`
+      } else {
+        document.querySelector(".resultado-peso").innerHTML = `
       IMC : ${imc} 
       
     `;
-    document.querySelector(".resultado-situacao").innerHTML = `
+        document.querySelector(".resultado-situacao").innerHTML = `
     Situação - Obesidade grau III ou Mórbida
     `
-    }
+      }
 
-    document.querySelector("#peso").value=""
-    document.querySelector("#altura").value=""
-  }
-})
+      document.querySelector("#peso").value = ""
+      document.querySelector("#altura").value = ""
+    }
+  })
